f7f9fce7ab62c5cdbf25d1a05950f98fbd10a2ea
Ginks <pissabruney@hotmail.com>
2017-10-20 16:38:46 +0200

    14ème
    Modification complète de la classe Expression

--- src/Expression.java
+++ src/Expression.java
@@ -1,62 +1,28 @@
+import java.util.LinkedList;
+
 public class Expression {
-    Inconnue x;
-    Nombre n;
-    String o;
-    Nombre multx;
+    private LinkedList<Object> expr;
 
-    /**
-     * Contruit une expression
-     * @param x
-     * @param n
-     * @param o
-     */
-    public Expression(Inconnue x, Nombre n, String o) {
-        this.x = x;
-        this.n = n;
-        this.o=o;
-        this.multx=null;
+    public Expression(LinkedList<Object> expr) {
+        this.expr = expr;
     }
 
-    /**
-     * Construir une expression avec un coefficient
-     * @param i
-     * @param x
-     * @param n
-     * @param o
-     */
-    public Expression(Nombre i, Inconnue x, Nombre n, String o){
-        this.x = x;
-        this.n = n;
-        this.o=o;
-        this.multx=i;
+    public LinkedList<Object> getExpr() {
+        return expr;
     }
 
-    public Nombre subt (Nombre a){
-            switch (o){
-                case "+":
-                    if(multx!=null)
-                        return n.plus(multx.fois(a));
-                    return n.plus(a);
-                case "-":
-                    if(multx!=null)
-                        return n.moins(multx.fois(a));
-                    return n.moins(a);
-                case "*":
-                    if(multx!=null)
-                        return n.fois(multx.fois(a));
-                    return n.fois(a);
-                case "/":
-                    if(multx!=null)
-                        return n.quotient(multx.fois(a));
-                    return n.quotient(a);
-        }
-        return null;
+    public Expression() {
+        this.expr = new LinkedList<>();
     }
 
     @Override
     public String toString() {
-        if(multx==null)
-            return "("+x+o+n+")";
-        return "("+multx+x+o+n+")";
+        String s="(";
+        for (Object e:this.expr
+             ) {
+                s+=e.toString();
+        }
+        s+=")";
+        return s;
     }
 }

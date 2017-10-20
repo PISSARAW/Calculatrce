5c40f99dabfc50c758f760e0dd176451a354bf10
Ginks <pissabruney@hotmail.com>
2017-10-20 13:36:26 +0200

    11ème
    Modification de Rationnel

--- src/Rationnel.java
+++ src/Rationnel.java
@@ -1,4 +1,53 @@
 public final class Rationnel implements Nombre{
+    private final Long num;
+    private final Long den;
+
+    private Rationnel(Long num, Long den) {
+        this.num = num;
+        this.den = den;
+    }
+
+    public static Rationnel faire(long n, long d){
+        try{
+            Rationnel r=null;
+            if(d!=0.0&&Rationnel.coprem(n, d)==1){
+                if(d<0){
+                    d=d+(-d*2);
+                    n=n-(n*2);
+                }
+                r = new Rationnel(n,d);
+                return r;
+            }
+            else
+                throw new Exception();
+        }
+        catch (Exception e){
+            System.out.println(" Nombre Rationnel impossible");
+            System.out.println(n + " n | d "+ d);
+        }
+        return null;
+    }
+
+    public static long coprem(long a, long b){
+        a=(a>0)?a:-a;
+        b=(b>0)?b:-b;
+        if (a==0)
+            return b;
+        return coprem(b%a,a);
+    }
+
+    @Override
+    public String toString() {
+        return ""+num+"/"+den;
+    }
+
+    public long getNum() {
+        return num;
+    }
+
+    public long getDen() {
+        return den;
+    }
 
     /**
      * Additione deux nombres
@@ -30,7 +79,11 @@ public final class Rationnel implements Nombre{
      */
     @Override
     public Nombre fois(Nombre n) {
-        return null;
+        if (n instanceof Flottant)
+            return Flottant.faire((this.num*((Flottant) n).getX())/(this.den*1));
+        else if (n instanceof Entier)
+            return Rationnel.faire(this.num*((Entier) n).getX(), this.den);
+        return Rationnel.faire(num*((Rationnel) n).getNum(), den*((Rationnel) n).getDen());
     }
 
     /**
@@ -41,6 +94,16 @@ public final class Rationnel implements Nombre{
      */
     @Override
     public Nombre quotient(Nombre n) {
-        return null;
+        return this.fois(n.inverse());
+    }
+
+    /**
+     * Inverse in nombre
+     *
+     * @return
+     */
+    @Override
+    public Nombre inverse() {
+        return Rationnel.faire(this.getDen(),this.getNum());
     }
 }
